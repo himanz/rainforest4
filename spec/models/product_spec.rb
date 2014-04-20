4e4dcda6fb2fac64f2707690fe222750a1c5a4ef
Jonathan Chang <jonoschang7@gmail.com>
2014-04-20 12:54:41 -0400

    Add test to Product

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Product do
+	it "must have a name" do
+		product = Product.new
+		product.name.should == nil
+		product.name.should_not == "something"
+		product.should_not be_valid
+	end
+end
